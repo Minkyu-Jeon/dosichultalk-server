ee53b7e35fc4e0206b7153d6801b68191f200fdd
Minkyu Jeon <minkyu@huray.net>
2017-03-30 21:15:48 +0900

    채팅 클라이언트 예제 생성

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const debug = require('debug')
 
 const app = express()
 const http = require('http').Server(app)
+
 const io = require('socket.io')(http)
 
 const routes = require('./routes/index')
@@ -29,3 +30,53 @@ app.use("/chat", chat);
 http.listen(app.get('port'), function() {
   console.log("Express server listening on port " + app.get('port'));
 })
+
+
+io.on('connection', function(socket){
+     
+    console.log('a user connected');
+     
+    socket.broadcast.emit('hi');
+     
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+     
+    socket.on('chat message', function(msg){
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    }); 
+ 
+});
+
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+ 
+// error handlers
+ 
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+ 
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+ 
